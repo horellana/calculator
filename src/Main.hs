1540edfa845c2285b0efcad2a4e587e10beb32de
Hector Orellana <pulga.p.29@gmail.com>
2015-08-22 04:41:03 -0300

    Everything seems to work.

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,6 @@
 module Main where
 
 import Text.Read
-import Data.Either
 
 data Function = Function { name :: String,
                            argc :: Int,
@@ -34,10 +33,35 @@ table = [("+", Function { name = "+",
                           argc = 2,
                           code = foldr div 1 })]
 
-eval :: String -> Stack -> Table -> Either String Stack
-eval f s t = case lookup f t of
+apply :: Function -> Stack -> Stack
+apply (Function _ argc code) stack = f code [] 0 stack
+  where
+    f :: ([Int] -> Int) -> [Int] -> Int -> Stack -> Stack
+    f fn args cont stack'
+      | cont == argc = push stack' (fn args) 
+      | otherwise = let (stack'', a) = pop stack'
+                    in f fn (a : args) (cont + 1) stack''
+
+call :: String -> Stack -> Table -> Either String Stack
+call f s t = case lookup f t of
   Just function -> Right $ apply function s
   Nothing -> Left $ "Undefined function: " ++ f
 
+loop :: [String] -> Stack -> Table -> Stack
+loop [] s _ = s
+loop (x:xs) stack table = case readMaybe x :: Maybe Int of
+  -- x == n, is a number, so add it to the stack
+  -- and go to the next element
+  Just n -> loop xs (push stack n) table
+  -- x is a string, so see if its a known function
+  -- call it, and go on 
+  Nothing -> case call x stack table of
+    Right (Stack t) -> loop xs (Stack t) table
+    Left e -> error e
+
 main :: IO ()
-main = undefined
+main = getContents
+       >>= (\a -> return $ loop (words a) (Stack []) table)
+       >>= print
+
+  
