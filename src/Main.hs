b3cf00d7e27f7007268d433a4bb6a7bddec7a3ba
Hector Orellana <pulga.p.29@gmail.com>
2015-08-22 05:33:12 -0300

    handle errors and use do notation.

--- src/Main.hs
+++ src/Main.hs
@@ -11,13 +11,15 @@ instance Show Function where
 
 data Stack = Stack [Int] deriving (Show)
 
+type Table = [(String, Function)]
+type Error = String
+
 push :: Stack -> Int -> Stack
 push (Stack s) n = Stack (n : s)
 
-pop :: Stack -> (Stack, Int)
-pop (Stack (x:xs)) = (Stack xs, x)
-
-type Table = [(String, Function)]
+pop :: Stack -> Either Error (Stack, Int)
+pop (Stack []) = Left "stack underflow"
+pop (Stack (x:xs)) = Right (Stack xs, x)
 
 table :: Table
 table = [("+", Function { name = "+",
@@ -33,35 +35,39 @@ table = [("+", Function { name = "+",
                           argc = 2,
                           code = foldr div 1 })]
 
-apply :: Function -> Stack -> Stack
+apply :: Function -> Stack -> Either Error Stack
 apply (Function _ argc code) stack = f code [] 0 stack
   where
-    f :: ([Int] -> Int) -> [Int] -> Int -> Stack -> Stack
+    f :: ([Int] -> Int) -> [Int] -> Int -> Stack -> Either Error Stack
     f fn args cont stack'
-      | cont == argc = push stack' (fn args) 
-      | otherwise = let (stack'', a) = pop stack'
-                    in f fn (a : args) (cont + 1) stack''
+      | cont == argc = Right $ push stack' (fn args) 
+      | otherwise = do (stack'', a) <- pop stack'
+                       f fn (a : args) (cont + 1) stack''
 
-call :: String -> Stack -> Table -> Either String Stack
+                  
+call :: String -> Stack -> Table -> Either Error Stack
 call f s t = case lookup f t of
-  Just function -> Right $ apply function s
+  Just function -> apply function s
   Nothing -> Left $ "Undefined function: " ++ f
 
-loop :: [String] -> Stack -> Table -> Stack
-loop [] s _ = s
+loop :: [String] -> Stack -> Table -> Either Error Stack
+loop [] s _ = Right s
 loop (x:xs) stack table = case readMaybe x :: Maybe Int of
   -- x == n, is a number, so add it to the stack
   -- and go to the next element
   Just n -> loop xs (push stack n) table
   -- x is a string, so see if its a known function
   -- call it, and go on 
-  Nothing -> case call x stack table of
-    Right (Stack t) -> loop xs (Stack t) table
-    Left e -> error e
+  Nothing -> do (Stack t) <- call x stack table
+                loop xs (Stack t) table
 
 main :: IO ()
-main = getContents
-       >>= (\a -> return $ loop (words a) (Stack []) table)
-       >>= print
+main = do
+  input <- getContents
+  case loop (words input) (Stack []) table of
+    Right (Stack s) -> print s
+    Left err -> print err
+
+         
 
   
