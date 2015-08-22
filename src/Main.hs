70bd0a6228394e6f234d866ecd0ef42f25ba079c
Hector Orellana <pulga.p.29@gmail.com>
2015-08-21 21:51:55 -0300

    function table

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,32 @@
 module Main where
 
+import Text.Read
+
 data Function = Function { name :: String,
                            args :: Int,
                            code :: [Int] -> Int }
 
 instance Show Function where
-  show (Function name args code) = "<Function " ++ name ++ ">"
+  show (Function name args _) = "<Function " ++ name ++ " : " ++ show args ++ ">"
 
 data Stack = Stack [Int] deriving (Show)
 
 apply :: Function -> Stack -> Stack
 apply (Function _ args code) (Stack stack) = Stack $ (code $ take args stack) : (drop args stack)
-                                                                                                
+
+table :: [(String, Function)]
+table = [("+", Function { name = "+",
+                          args = 2,
+                          code = sum }),
+         ("-", Function { name = "-",
+                          args = 2,
+                          code = foldr (-) 0}),
+         ("*", Function { name = "*",
+                          args = 2,
+                          code = product }),
+         ("/", Function { name = "/",
+                          args = 2,
+                          code = foldr div 1 })]
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = undefined
